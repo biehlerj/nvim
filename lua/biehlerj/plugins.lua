10f0e3d17fe203f07488772e3d6d750e4ae3a976
Jacob Biehler <jacob@biehlerj.xyz>
2024-09-23 23:01:00 -0700

    fix(neotree): Autoclosing neo-tree on file open

--- lua/biehlerj/plugins.lua
+++ lua/biehlerj/plugins.lua
@@ -268,7 +268,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
-			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+			"3rd/image.nvim",     -- Optional image support in preview window: See `# Preview Mode` for more information
 			{
 				's1n7ax/nvim-window-picker',
 				version = '2.*',
@@ -301,13 +301,24 @@ return {
 			-- 	{ text = "󰌵", texthl = "DiagnosticSignHint" })
 
 			require("neo-tree").setup({
+				event_handlers = {
+					{
+						event = "file_open_requested",
+						handler = function()
+							-- auto close
+							-- vim.cmd("Neotree close")
+							-- OR
+							require("neo-tree.command").execute({ action = "close" })
+						end
+					},
+				},
 				close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
 				popup_border_style = "rounded",
 				enable_git_status = true,
 				enable_diagnostics = true,
 				open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-				sort_case_insensitive = false,                             -- used when sorting files and directories in the tree
-				sort_function = nil,                                       -- use a custom function for sorting files and directories in the tree
+				sort_case_insensitive = false,                         -- used when sorting files and directories in the tree
+				sort_function = nil,                                   -- use a custom function for sorting files and directories in the tree
 				-- sort_function = function (a,b)
 				--       if a.type == b.type then
 				--           return a.path > b.path
@@ -492,11 +503,11 @@ return {
 						},
 					},
 					follow_current_file = {
-						enabled = false,          -- This will find and focus the file in the active buffer every time
+						enabled = false,     -- This will find and focus the file in the active buffer every time
 						--               -- the current file is changed while the tree is open.
-						leave_dirs_open = false,  -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+						leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
 					},
-					group_empty_dirs = false,     -- when true, empty folders will be grouped together
+					group_empty_dirs = false, -- when true, empty folders will be grouped together
 					hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
 					-- in whatever position is specified in window.position
 					-- "open_current",  -- netrw disabled, opening a directory opens within the
